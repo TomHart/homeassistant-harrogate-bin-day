c8d81e8c8df5f4fd105dccaa75e67082df19c724
Tom Hart <thomas.hart@sky.uk>
2022-02-24 22:11:40 +0000

    Swap home to user

--- custom_components/harrogate_bin/config_flow.py
+++ custom_components/harrogate_bin/config_flow.py
@@ -17,34 +17,13 @@ PLATFORM_SCHEMA = vol.Schema(
     }
 )
 
-# def validate_path(path: str) -> None:
-#     """Validates a GitHub repo path.
-#
-#     Raises a ValueError if the path is invalid.
-#     """
-#     if len(path.split("/")) != 2:
-#         raise ValueError
-#
-#
-# async def validate_auth(access_token: str, hass: core.HomeAssistant) -> None:
-#     """Validates a GitHub access token.
-#
-#     Raises a ValueError if the auth token is invalid.
-#     """
-#     session = async_get_clientsession(hass)
-#     gh = GitHubAPI(session, "requester", oauth_token=access_token)
-#     try:
-#         await gh.getitem("repos/home-assistant/core")
-#     except BadRequest:
-#         raise ValueError
-
 
 class BinDayCustomConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Bin Day Custom config flow."""
 
     data: Optional[Dict[str, Any]]
 
-    async def async_step_home(self, user_input: Optional[Dict[str, Any]] = None):
+    async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
         """Invoked when a user initiates a flow via the user interface."""
         errors: Dict[str, str] = {}
         if user_input is not None:
@@ -52,5 +31,5 @@ class BinDayCustomConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="Bin Day", data=self.data)
 
         return self.async_show_form(
-            step_id="home", data_schema=PLATFORM_SCHEMA, errors=errors
+            step_id="user", data_schema=PLATFORM_SCHEMA, errors=errors
         )
