64bbda0ae5bc3cd0ae9646d88633bf45e7e583c4
Tom Hart <1374434+TomHart@users.noreply.github.com>
2024-05-09 23:19:37 +0100

    Added note (#3)

--- custom_components/harrogate_bin/const.py
+++ custom_components/harrogate_bin/const.py
@@ -4,3 +4,4 @@ DOMAIN = "harrogate_bin"
 
 ATTR_TYPE: Final = "type"
 ATTR_HOURS_UNTIL: Final = "hours_until"
+ATTR_NOTE: Final = "note"
--- custom_components/harrogate_bin/parser.py
+++ custom_components/harrogate_bin/parser.py
@@ -9,7 +9,7 @@ class BinDay:
     def __init__(self, uprn: int):
         self.uprn = uprn
 
-    def get_next_bin_day(self) -> tuple[datetime, str]:
+    def get_next_bin_day(self) -> tuple[datetime, str, str]:
         parsed_html = BeautifulSoup(self.get_page_html(), features="html.parser")
         rows = parsed_html.body.find_all('table', attrs={'class': 'hbcRounds'})
 
@@ -18,10 +18,14 @@ class BinDay:
 
         row_index = len(rows) - 1
         timestamp = self.get_bin_day_from_row(rows[row_index], 0)
-        if timestamp <= datetime.now():
-            return self.get_bin_day_from_row(rows[row_index], 1), self.get_bin_type(rows[row_index], 1)
+        bin_type = self.get_bin_type(rows[row_index], 0)
+        note = self.get_note(rows[row_index], 0)
 
-        return timestamp, self.get_bin_type(rows[row_index], 0)
+        if timestamp <= datetime.now():
+            timestamp = self.get_bin_day_from_row(rows[row_index], 1)
+            bin_type = self.get_bin_type(rows[row_index], 1)
+
+        return timestamp, bin_type, note
 
     def get_page_html(self) -> str:
         return requests.get(
@@ -36,3 +40,8 @@ class BinDay:
     @staticmethod
     def get_bin_type(row, cell) -> str:
         return row.find_all('th')[cell].get_text().lstrip('\\r\\n').strip()
+
+    @staticmethod
+    def get_note(row, cell) -> str:
+        next_day = row.find_all('td')[cell].get_text().lstrip('\\r\\n').strip()[15:]
+        return next_day.strip(' ')
